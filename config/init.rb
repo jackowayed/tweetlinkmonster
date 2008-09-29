7ee41c9a3cd59e89170070251057afa410be3d44
Daniel Jackoway <danjdel@gmail.com>
2008-09-29 15:51:21 -0400

    reordered dependencies

--- config/init.rb
+++ config/init.rb
@@ -57,8 +57,7 @@ Merb::BootLoader.after_app_loads do
 
   # dependency "magic_admin" # this gem uses the app's model classes
 end
-dependencies "merb_helpers", "merb-assets", "twitter", "htmlentities"
-, "dm-ar-finders","dm-validations"
+dependencies "merb_helpers", "merb-assets", "twitter", "htmlentities","dm-validations", "dm-ar-finders"
 #
 # ==== Set up your ORM of choice
 #
