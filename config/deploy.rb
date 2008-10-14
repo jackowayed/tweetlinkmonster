b8a5bae4fe586bc4306e2e91f5e709c85e225bd1
Daniel Jackoway <danjdel@gmail.com>
2008-10-13 20:30:44 -0400

    messed w/ deploy a little but commented

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,12 +14,13 @@ set :use_sudo, false
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-default_run_options[:pty] = true
+#default_run_options[:pty] = true
 
 set :user, "tlm"
-#set :git_user, "danieljdel"
+set :git_user, "danieljdel"
 
 set :scm, :git
+#set :deploy_via, :remote_cache
 set :deploy_via, :copy
 set :copy_strategy, :export
 
