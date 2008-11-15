f38bebaeb84ecbf817de7a251a1438e82185e93a
Daniel Jackoway <danjdel@gmail.com>
2008-11-14 20:41:02 -0500

    fixed bug, trying to call a Tweet method on a Twitter::Status

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,7 +55,7 @@ class User
       end
       tweets.reverse.each do |t|
         next unless t.user.screen_name != self.username && Tweet.find_website(t.text)
-        title = self.find_site_title(t.website)
+        title = self.find_site_title(Tweet.find_website(t.text))
         tweet = Tweet.new({:user_id => self.id, :text => t.text, :created_at => t.created_at, :author => t.user.name, :title => title})
         tweet.save if t.user.screen_name != self.username && tweet.website
       end
