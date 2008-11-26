0c310d43198e588c492655662671a885ca7881f4
Daniel Jackoway <danjdel@gmail.com>
2008-11-25 22:28:41 -0500

    fixed bug w/ www sites

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,9 @@
 module Merb
   module UsersHelper
-
+    
+    def httpified_link(url)
+      ((/.{3,5}:\/\//=~url)==0)?(url):("http://#{url}")
+    end
+      
   end
 end # Merb
---
+++ remote_update_tweets.sh
@@ -0,0 +1,1 @@
+ssh tlm@tweetlinkmonster.com 'cd ~/apps/tweetlinkmonster/current; ./update_tweets.sh > ~/cronlog.log'
