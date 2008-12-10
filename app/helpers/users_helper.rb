98827b86ab103a39bfe07c8faae3f203724fd29d
Daniel Jackoway <daniel@daniel-laptop.(none)>
2008-12-09 22:24:49 -0500

    username: feature view/helper changes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,6 +4,8 @@ module Merb
     def httpified_link(url)
       ((/.{3,5}:\/\//=~url)==0)?(url):("http://#{url}")
     end
-      
+    def tweet_link(user, id)
+      link_to "#{user}:", "http://www.twitter.com/#{user}/status/#{id}"
+    end
   end
 end # Merb
