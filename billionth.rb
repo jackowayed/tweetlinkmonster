7255af7d824d1e0b40b4f7ce76aef51144e44828
Daniel Jackoway <daniel@daniel-laptop.(none)>
2009-03-20 11:02:09 -0400

    billionth tweet long since past. not OAuth related at all

--- billionth.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-require 'rubygems'
-require 'twitter'
-x = Twitter::Base.new('tweetlinkmonstr', 'getmetweetlinks!')
-x.update("Want billionth. Spamming for tweetlinkmonster.com! Check out http://tinyurl.com/4qbuyb to get an atom feed of the links your friends tweet") while true
